1cdd82d2d45080452eadc950070bea9e1ac7fab8
Lance C. Jackson <lj126609@gmail.com>
2016-06-02 22:28:10 -0700

    Removed comments

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 	$('.sign').click(function() {
 		if ( $(this).hasClass('current') ) {
-			//console.log("Should happen");
 			$('.sign').removeClass('current');
 			$('.scope').hide();
 		}
 		else {
-			//console.log("Shouldn't happen");
 			$('.sign').removeClass('current');
 			$('.scope').hide();
 			$(this).addClass('current');
